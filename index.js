ec54cf0e927a568227f826e196a06786fedfe7c8
HHJ74 <gpgus0704@gmail.com>
2024-10-04 12:16:49 +0900

    change index

--- index.js
+++ index.js
@@ -9,172 +9,182 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
-const isDevelopment =() =>{
-  return process.env.NODE_ENV === 'development'
+const isDevelopment = () => {
+  return process.env.NODE_ENV === 'development';
 };
 
-const pythonExePath = isDevelopment() 
-  ? path.join(__dirname, 'recom_env', 'bin', 'python3')
-  : path.join(
-  '/home/ubuntu/miniconda',
-  'envs',
-  'myenv',
-  'bin',
-  'python3'
-)
-
-// const pythonPath = path.join(__dirname, 'recom_env', 'bin', 'python3');
-// const pythonPath = path.join(__dirname, 'venv', 'bin', 'python3');
-const pythonPath = path.join(
-  '/home/ubuntu/miniconda',
-  'envs',
-  'myenv',
-  'bin',
-  'python3'
-);
-
+const pythonExePath = isDevelopment()
+  ? path.join( __dirname, 'recom_env', 'bin', 'python3')
+  : path.join('/home/ubuntu/miniconda', 'envs', 'myenv', 'bin', 'python3');
 
 app.get('/', (req, res) => {
   res.send('Hello from Node server!');
 });
 
 app.get('/random/:count', (req, res) => {
-  const scriptPath = path.join(__dirname, 'resolver.py');
-  
-  const count=req.params.count;
-  const result = spawn(pythonExePath, [scriptPath, "random", count]);
+  try {
+    const scriptPath = path.join(__dirname, 'resolver.py');
 
-  let responseData = '';
+    const count = req.params.count;
+    const result = spawn(pythonExePath, [scriptPath, 'random', count]);
 
-  result.stdout.on('data', function (data) {
-    responseData += data.toString();
-  });
+    let responseData = '';
 
-  result.on('close', (code) => {
-    if (code === 0) {
-      const jsonResponse = JSON.parse(responseData);
-      res.status(200).json(jsonResponse);
-    } else {
-      res.status(500).json({ error: `Child process exited with code ${code}` });
-    }
-  });
+    result.stdout.on('data', function (data) {
+      responseData += data.toString();
+    });
 
-  result.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-  });
+    result.on('close', (code) => {
+      if (code === 0) {
+        const jsonResponse = JSON.parse(responseData);
+        res.status(200).json(jsonResponse);
+      } else {
+        res
+          .status(500)
+          .json({ error: `Child process exited with code ${code}` });
+      }
+    });
+
+    result.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+    });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 });
 
 app.get('/latest/:count', (req, res) => {
-  const scriptPath = path.join(__dirname, 'resolver.py');
+  try {
+    const scriptPath = path.join(__dirname, 'resolver.py');
 
+    const count = req.params.count;
+    const result = spawn(pythonExePath, [scriptPath, 'latest', count]);
 
-  const count=req.params.count;
-  const result = spawn(pythonExePath, [scriptPath, "latest", count]);
+    let responseData = '';
 
-  let responseData = '';
+    result.stdout.on('data', function (data) {
+      responseData += data.toString();
+    });
 
-  result.stdout.on('data', function (data) {
-    responseData += data.toString();
-  });
+    result.on('close', (code) => {
+      if (code === 0) {
+        const jsonResponse = JSON.parse(responseData);
+        res.status(200).json(jsonResponse);
+      } else {
+        res
+          .status(500)
+          .json({ error: `Child process exited with code ${code}` });
+      }
+    });
 
-  result.on('close', (code) => {
-    if (code === 0) {
-      const jsonResponse = JSON.parse(responseData);
-      res.status(200).json(jsonResponse);
-    } else {
-      res.status(500).json({ error: `Child process exited with code ${code}` });
-    }
-  });
-
-  result.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-  });
+    result.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+    });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 });
 
 app.get('/genres/:genre/:count', (req, res) => {
-  const scriptPath = path.join(__dirname, 'resolver.py');
-  
+  try {
+    const scriptPath = path.join(__dirname, 'resolver.py');
 
-  const genre=req.params.genre;
-  const count=req.params.count;
-  const result = spawn(pythonExePath, [scriptPath, "genres", genre, count]);
+    const genre = req.params.genre;
+    const count = req.params.count;
+    const result = spawn(pythonExePath, [scriptPath, 'genres', genre, count]);
 
-  let responseData = '';
+    let responseData = '';
 
-  result.stdout.on('data', function (data) {
-    responseData += data.toString();
-  });
+    result.stdout.on('data', function (data) {
+      responseData += data.toString();
+    });
 
-  result.on('close', (code) => {
-    if (code === 0) {
-      const jsonResponse = JSON.parse(responseData);
-      res.status(200).json(jsonResponse);
-    } else {
-      res.status(500).json({ error: `Child process exited with code ${code}` });
-    }
-  });
+    result.on('close', (code) => {
+      if (code === 0) {
+        const jsonResponse = JSON.parse(responseData);
+        res.status(200).json(jsonResponse);
+      } else {
+        res
+          .status(500)
+          .json({ error: `Child process exited with code ${code}` });
+      }
+    });
 
-  result.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-  });
+    result.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+    });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 });
 
 app.get('/item-based/:item', (req, res) => {
-  const scriptPath = path.join(__dirname, 'recommender.py');
+  try {
+    const scriptPath = path.join(__dirname, 'recommender.py');
 
-  const item=req.params.item;
-  const result = spawn(pythonExePath, [scriptPath, "item-based", item]);
+    const item = req.params.item;
+    const result = spawn(pythonExePath, [scriptPath, 'item-based', item]);
 
-  let responseData = '';
+    let responseData = '';
 
-  result.stdout.on('data', function (data) {
-    responseData += data.toString();
-  });
+    result.stdout.on('data', function (data) {
+      responseData += data.toString();
+    });
 
-  result.on('close', (code) => {
-    if (code === 0) {
-      const jsonResponse = JSON.parse(responseData);
-      res.status(200).json(jsonResponse);
-    } else {
-      res.status(500).json({ error: `Child process exited with code ${code}` });
-    }
-  });
+    result.on('close', (code) => {
+      if (code === 0) {
+        const jsonResponse = JSON.parse(responseData);
+        res.status(200).json(jsonResponse);
+      } else {
+        res
+          .status(500)
+          .json({ error: `Child process exited with code ${code}` });
+      }
+    });
 
-  result.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-  });
+    result.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+    });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 });
 
 app.post('/user-based', (req, res) => {
-  const scriptPath = path.join(__dirname, 'recommender.py');
+  try {
+    const scriptPath = path.join(__dirname, 'recommender.py');
 
-  const inputRatingDict=req.body;
-  const result = spawn(pythonExePath, [scriptPath, "user-based"]);
+    const inputRatingDict = req.body;
+    const result = spawn(pythonExePath, [scriptPath, 'user-based']);
 
-  let responseData = '';
+    let responseData = '';
 
-  // 파이썬 스크립트로 JSON 데이터를 전달
-  result.stdin.write(JSON.stringify(inputRatingDict));
-  result.stdin.end(); //더 이상 데이터가 없으면 전달 끝
+    // 파이썬 스크립트로 JSON 데이터를 전달
+    result.stdin.write(JSON.stringify(inputRatingDict));
+    result.stdin.end(); // 더 이상 데이터가 없으면 전달 끝
 
-  result.stdout.on('data', function (data) {
-    responseData += data.toString();
-  });
+    result.stdout.on('data', function (data) {
+      responseData += data.toString();
+    });
 
-  result.on('close', (code) => {
-    if (code === 0) {
-      const jsonResponse = JSON.parse(responseData);
-      res.status(200).json(jsonResponse);
-    } else {
-      res.status(500).json({ error: `Child process exited with code ${code}` });
-    }
-  });
+    result.on('close', (code) => {
+      if (code === 0) {
+        const jsonResponse = JSON.parse(responseData);
+        res.status(200).json(jsonResponse);
+      } else {
+        res
+          .status(500)
+          .json({ error: `Child process exited with code ${code}` });
+      }
+    });
 
-  result.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-  });
+    result.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+    });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 });
-
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
